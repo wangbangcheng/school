88dfcdad57ee2b71e0d5c9238f26f2749e79a6bf
Administrator <Administrator@CY-20181116WQZJ>
2019-04-01 16:30:35 +0800

    111

--- graduationDemo/src/main/java/com/wbc/graduation/controller/PageController.java
+++ graduationDemo/src/main/java/com/wbc/graduation/controller/PageController.java
@@ -1,16 +1,22 @@
 package com.wbc.graduation.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.wbc.graduation.exception.Md5EncodeException;
+import com.wbc.graduation.util.MD5diyUtils;
 
 
 @Controller
 @RequestMapping("/page")
 public class PageController {
-	
+	@Autowired
+	private MD5diyUtils MD5diyUtils;
+	private static int i = 0;
 	@RequestMapping("/login")
     public String Login() {
-        return "Login";
+		return "Login";
     }
 	
 	@RequestMapping("/register")
